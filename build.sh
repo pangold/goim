ef4f003ce13a458b1c7b2970e86059c5f40b334e
king <pangold@163.com>
2020-01-12 13:53:32 +0800

    build.sh

--- build.sh
+++ build.sh
@@ -1,43 +1,44 @@
 #!/bin/bash
 
- check folder
-if test ! -d "build"; then
-  mkdir -p build
-fi
 
-# check output name
-if test "$1" = ""; then
-  output="goim"
-else
-  output="$1"
-fi
+output=goim
+build_path=build
+api_path=./protocol/api.proto
+message_path=./protocol/message.proto
+
+# check build folder
+check_folder() {
+  if [ ! -d $build_path ]; then
+    mkdir -p $build_path
+  fi
+}
 
 # compile protobuf files
 compile_protobuf_files() {
   if [ -e $1 ]; then
     echo "compling $1"
-    protoc --proto_path=$(dirname $1) --go_out=$(dirname $1) $(basename $1)
+    protoc -I=$(dirname $1) --go_out=$(dirname $1) $(basename $1)
   fi
   if [ -e $1 -a -e $2 ]; then
     echo "compling $2"
-    protoc --proto_path=$(dirname $1) --proto_path=$(dirname $2) --go_out=plugins=grpc:$(dirname $2) $(basename $2)
+    protoc -I $(dirname $1) -I $(dirname $2) --go_out=plugins=grpc:$(dirname $2) $(basename $2)
   fi
 }
 
+# compile this go project
 complile_project() {
-  if [ "$1" = "linux" ]; then
+  if [ "$2" = "linux" ]; then
     echo "compiling linux binary ..."
-    env GOOS=linux GOARCH=amd64 go build -o build/$2 main.go
+    env GOOS=linux GOARCH=amd64 go build -o $build_path/$1 main.go
   else
     echo "compiling current platform binary ..."
-    go build -o build/$2 main.go
+    go build -o $build_path/$1 main.go
   fi
 }
 
-compile_protobuf_files ./codec/protobuf/message.proto ./api/grpc/im.proto
-complile_project $2
-
-# compile
-
+#
+check_folder
+compile_protobuf_files $message_path $api_path
+complile_project $output $1
 
 # docker image
